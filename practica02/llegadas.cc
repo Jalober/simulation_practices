56a17318d4a02ad6c2a1f28a68601ac40f69abef
ns3 <ns3@arodriguezdev.(none)>
2015-10-13 23:18:42 +0200

    fixed various errors. Now it works

--- practica02/llegadas.cc
+++ practica02/llegadas.cc
@@ -16,18 +16,18 @@ Llegadas::Llegadas (double tasa)
   // ... y ajustamos su valor medio
   tEntreLlegadas->SetAttribute ("Mean", DoubleValue (mean));  
   // Programamos la primera llegada. //HAY QUE PASAR UN TIME COMO PRIMER PARAMETRO
-  Simulator::Schedule (Time::(tEntreLlegadas->GetValue()), &Llegadas::NuevaLlegada, this, Simulator::Now.GetDouble());
+  Simulator::Schedule (Seconds(tEntreLlegadas->GetValue()), &Llegadas::NuevaLlegada, this, Simulator::Now ().GetSeconds());
 }
 
 void
 Llegadas::NuevaLlegada (double tiempo)
 {
   // Calculamos el tiempo transcurrido desde la última llegada
-  double t = Simulator::Now.GetDouble() - tiempo;
+  double t = Simulator::Now ().GetSeconds() - tiempo;
   // Presentamos el tiempo transcurrido entre llegadas.
   NS_LOG_INFO ("Tiempo transcurrido entre llegadas: " << t);
   // Programamos la siguiente llegada
-  Simulator::Schedule (tEntreLlegadas->GetValue(), &Llegadas::NuevaLlegada, this, Simulator::Now.GetDouble());
+  Simulator::Schedule (Seconds(tEntreLlegadas->GetValue()), &Llegadas::NuevaLlegada, this, Simulator::Now ().GetSeconds());
   // Acumulamos el valor del intervalo
   acumulador.Update(t);
 }
@@ -37,5 +37,5 @@ double
 Llegadas::Media ()
 {
   // Devolvemos el valor medio de las muestras recogidas.
-  acumulador.Mean()
+  return acumulador.Mean();
 }
