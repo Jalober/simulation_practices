5c2572b994d9681a7b64d1a04450014eb6ca6f79
Alberto Rodríguez de la Cruz <arodriguez.ext@redborder.net>
2015-11-16 18:13:16 +0100

    Separacion de simulador.cc en funcion main y funcion simulador

--- practica04/simulador.cc
+++ practica04/simulador.cc
@@ -7,38 +7,23 @@
 #include "Enlace.h"
 #include "Observador.h"
 
+#define T_STUDENT_VALUE 1.8331
+
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("Practica04");
 
 
-int
-main (int argc, char *argv[])
-{
-  Time::SetResolution (Time::US);
-
-  // Parámetros de la simulación
-  Time     trtx       = Time("6ms");
-  uint32_t tamPaquete = uint32_t(121);
-  Time     rprop      = Time("200us");
-  DataRate vtx        = DataRate("1000kbps");
-  uint8_t  tamVentana = 6;
-  double   errorRate  = double(0.01);
-
-  //Obtencion de parametros por linea de comandos
-  CommandLine cmd;
-  cmd.AddValue("window", "tamanio de la ventana de transmision", tamVentana);
-  cmd.AddValue("delay", "retardo de propagacion del enlace", rprop);
-  cmd.AddValue("rate", "capacidad de transmision en el canal", vtx);
-  cmd.AddValue("pktSize", "tamanio de la SDU del nivel de enlace", tamPaquete);
-  cmd.AddValue("wait", "tiempo de espera para la retransmision", trtx);  
-  cmd.Parse(argc, argv);
+double simulacion (Time trtx, uint32_t tamPaquete, Time rprop, DataRate vtx, uint8_t tamVentana, double errorRate) 
+{  
   
+  
+
   // Definimos el modelo de errores
   Ptr<ErrorModel> errorModel = CreateObject<RateErrorModel> ();
   errorModel->SetAttribute("ErrorUnit", StringValue ("ERROR_UNIT_PACKET"));
   errorModel->SetAttribute("ErrorRate", DoubleValue (errorRate));
-  
+
   // Configuramos el escenario:
   PointToPointHelper escenario;
   escenario.SetChannelAttribute ("Delay", TimeValue (rprop));
@@ -54,7 +39,7 @@ main (int argc, char *argv[])
   NetDeviceContainer dispositivos = escenario.Install (nodos);
  
   // Habilitamos la creacion de pcaps
-  escenario.EnablePcapAll("practica04");
+  // escenario.EnablePcapAll("practica04");
     
   Observador observador(vtx, Time("5s"), tamVentana);
   // Suscribimos la traza de paquetes correctamente asentidos de la primera aplicacion.
@@ -92,12 +77,64 @@ main (int argc, char *argv[])
 
   NS_LOG_FUNCTION  ("Total paquetes correctos"  << totalPaquetes);
   NS_LOG_FUNCTION  ("Total paquetes erroneos"   << totalErroneos);
-
+  
+  //Calculo de estadisticos
   DataRate cef  = observador.GETCef(); 
   double   rend = observador.GETRend();  
-
+  
   NS_LOG_INFO ("Cef: " << cef);
   NS_LOG_INFO ("Rend: " << rend);
+
+  return rend;
+}
+
+int
+main (int argc, char *argv[])
+{
+  Time::SetResolution (Time::US);
+
+  uint32_t seed = 1; //Semilla a utilizar
+
+  // Parámetros de la simulación
+  Time     trtx       = Time ("6ms");
+  uint32_t tamPaquete = uint32_t (121);
+  Time     rprop      = Time ("200us");
+  DataRate vtx        = DataRate ("1000kbps");
+  uint8_t  tamVentana = 6;
+  double   errorRate  = double (0);
+
+  //Obtencion de parametros por linea de comandos
+  CommandLine cmd;
+  cmd.AddValue ("window", "tamanio de la ventana de transmision", tamVentana);
+  cmd.AddValue ("delay", "retardo de propagacion del enlace", rprop);
+  cmd.AddValue ("rate", "capacidad de transmision en el canal", vtx);
+  cmd.AddValue ("pktSize", "tamanio de la SDU del nivel de enlace", tamPaquete);
+  cmd.AddValue ("wait", "tiempo de espera para la retransmision", trtx);  
+  cmd.Parse (argc, argv);
+
+  Gnuplot plot;
+  std::ostringstream rotulo;
+  plot.SetTitle ("Rendimiento en funcion de la probabilidad de error de paquete");
+  plot.SetLegend ("perror", "rend");+
+  
+  Gnuplot2dDataset dataset;
+  dataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+  dataset.SetErrorBars(Gnuplot2dDataset::Y);
+
+  for (errorRate = 0; errorRate <= 0.5; errorRate += 0.05) {
+    Average <double> rendimientos; 
+    for (int i = 0; i < 10; i++) {
+      //Modificamos la semilla de la simulacion
+      SeedManager::SetRun (seed++);
+      double rendimiento = simulacion (trtx, tamPaquete, rprop, vtx, tamVentana, errorRate);      
+      rendimientos.Update (rendimiento);
+    }
+    double rendimientoMedio = rendimientos.Mean();
+    double z = T_STUDENT_VALUE * std::sqrt (rendimientos.Var () / 10);
+    rendMedio.Add (errorRate, rendimientoMedio, 2 * z);    
+    NS_LOG_INFO ("Para errorRate = " << errorRate << " --> "
+       << rendimientoMedio - z << " < rendimiento < " << rendimientoMedio + <);  
+  } 
   
   return 0;
 }
