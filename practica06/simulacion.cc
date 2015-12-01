540e5d6b9cd81c948ec34d7b18b86cd13c48f176
Alberto Rodríguez de la Cruz <arodriguez.ext@redborder.net>
2015-12-01 11:17:27 +0100

    P06: inicio de practica

---
+++ practica06/simulacion.cc
@@ -0,0 +1,126 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+// Default Network Topology
+//
+//       10.1.1.0
+// n0 -------------- n1   n2   n3   n4
+//    point-to-point  |    |    |    |
+//                    ================
+//                      LAN 10.1.2.0
+
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("Practica06");
+
+int 
+main (int argc, char *argv[])
+{
+  GlobalValue::Bind("ChecksumEnabled", BooleanValue(true));
+  Time::SetResolution (Time::US);
+
+  uint32_t nCsma = 3;
+
+  CommandLine cmd;
+  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+  cmd.Parse (argc,argv);
+
+  nCsma = nCsma == 0 ? 1 : nCsma;
+
+  // Nodos que pertenecen al enlace punto a punto
+  NodeContainer p2pNodes;
+  p2pNodes.Create (2);
+
+  // Nodos que pertenecen a la red de área local
+  // Como primer nodo añadimos el encaminador que proporciona acceso
+  //      al enlace punto a punto.
+  NodeContainer csmaNodes;
+  csmaNodes.Add (p2pNodes.Get (1));
+  csmaNodes.Create (nCsma);
+
+  // Instalamos el dispositivo en los nodos punto a punto
+  PointToPointHelper pointToPoint;
+  NetDeviceContainer p2pDevices;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("2Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  p2pDevices = pointToPoint.Install (p2pNodes);
+
+  // Instalamos el dispositivo de red en los nodos de la LAN
+  CsmaHelper csma;
+  NetDeviceContainer csmaDevices;
+  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+  csmaDevices = csma.Install (csmaNodes);
+
+  // Instalamos la pila TCP/IP en todos los nodos
+  InternetStackHelper stack;
+  stack.Install (p2pNodes.Get (0));
+  stack.Install (csmaNodes);
+
+  // Asignamos direcciones a cada una de las interfaces
+  // Utilizamos dos rangos de direcciones diferentes:
+  //    - un rango para los dos nodos del enlace
+  //      punto a punto
+  //    - un rango para los nodos de la red de área local.
+  Ipv4AddressHelper address;
+  Ipv4InterfaceContainer p2pInterfaces;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  p2pInterfaces = address.Assign (p2pDevices);
+  Ipv4InterfaceContainer csmaInterfaces;
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  csmaInterfaces = address.Assign (csmaDevices);
+
+  // Calculamos las rutas del escenario. Con este comando, los
+  //     nodos de la red de área local definen que para acceder
+  //     al nodo del otro extremo del enlace punto a punto deben
+  //     utilizar el primer nodo como ruta por defecto.
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  
+  // Establecemos un sumidero para los paquetes en el puerto 9 del nodo
+  //     aislado del enlace punto a punto
+  uint16_t port = 9;
+  PacketSinkHelper sink ("ns3::UdpSocketFactory",
+                         Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+  ApplicationContainer app = sink.Install (p2pNodes.Get (0));
+
+  // Instalamos un cliente OnOff en uno de los equipos de la red de área local
+  OnOffHelper clientes ("ns3::UdpSocketFactory",
+                        Address (InetSocketAddress (p2pInterfaces.GetAddress (0), port)));
+  
+  ApplicationContainer clientApps = clientes.Install (csmaNodes.Get (nCsma));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  // Activamos las trazas pcap en las dos interfaces del nodo de enlace
+  pointToPoint.EnablePcap ("practica06", p2pDevices.Get (1));
+  csma.EnablePcap ("practica06", csmaDevices.Get (0), true);
+
+  // Lanzamos la simulación
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  return 0;
+}
